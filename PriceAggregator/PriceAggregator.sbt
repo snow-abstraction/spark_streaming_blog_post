7d7b19d37facfa823aa078dee42159d033b5ff2a
Douglas Wayne Potter <douglaspotter@gmail.com>
2014-11-30 19:26:42 +0100

    initial version before first blog post

---
+++ PriceAggregator/PriceAggregator.sbt
@@ -0,0 +1,8 @@
+name := "PriceAggregator"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.10" % "1.1.0"
+
---
+++ PriceAggregator/src/main/scala/PriceAggregator.scala
@@ -0,0 +1,39 @@
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._
+
+object PriceAggregator {
+  def main(args: Array[String]) {
+    if (args.length < 3) {
+      val usage = ("Usage: PriceAggregator <hostname> <port>"
+        ++ " <batch interval in ms>")
+      System.err.println(usage)
+      System.exit(1)
+    }
+    val sparkConf = new SparkConf().setAppName("PriceAggregator")
+    val batchInterval = args(2).toInt
+    val ssc = new StreamingContext(sparkConf, Milliseconds(batchInterval))
+    ssc.checkpoint("./checkpoints")
+
+    val lines = ssc.socketTextStream(args(0), args(1).toInt)
+    val idAndPriceStrs = lines.map(_.split(","))
+    val idAndPricesStream = 
+      idAndPriceStrs.map(x => (x(0).toInt, x(1).toDouble))
+
+    val idAndPricesState = 
+      idAndPricesStream.updateStateByKey[Double](dropOldForFirstNewPrice)
+    idAndPricesState.print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+  val dropOldForFirstNewPrice =
+    (newPrices: Seq[Double], oldPrice: Option[Double]) => {
+      val newPrice = newPrices.find(_ != Double.NaN)
+      newPrice match {
+        case Some(price) => newPrice
+        case _ => Some(oldPrice.getOrElse(Double.NaN))
+      }
+    }
+
+}
